c0b7e32412e4dab277622d4f1b4f3bc043622f34
daniel vasquez <danieleduardovasquezgarcia@gmail.com>
2021-10-19 15:49:09 -0400

    ultimo sin stylo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  #resources :articles
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # get 'articles/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'articles#index', as: :articles
+  post 'articles', to: 'articles#create'
+  get 'articles/new', to: 'articles#new', as: :newarticle
+  get 'articles/:id/edit', to: 'articles#edit', as: :ediarticle
+  get 'articles/:id', to: 'articles#show', as: :article
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
 end
